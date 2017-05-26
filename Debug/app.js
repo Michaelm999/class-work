a8982cd94f44a7b4466f70710c776a86691a99e5
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-05-26 12:34:33 -0700

    Week 1 work

---
+++ Debug/app.js
@@ -0,0 +1,18 @@
+
+var count = 0;
+
+alert("hi");
+
+console.log("Hello");
+
+setTimeout(function(){
+  alert("Loaded");
+}, 0);
+debugger
+setTimeout(function(){
+  alert("Hello");
+}, 500);
+
+setTimeout(function(){
+  alert("ROAR");
+}, 2000);
