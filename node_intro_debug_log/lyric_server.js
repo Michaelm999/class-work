b639db77f72634c64cfdb41bc3846225c20a1202
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-05 17:15:10 -0700

    nodework

--- node_intro_debug_log/lyric_server.js
+++ node_intro_debug_log/lyric_server.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
 const http = require('http')
 
+var data = fs.readFileSync('baby.txt')
+var lyrics = fs.readFileSync('yellow.txt')
+var espana = fs.readFileSync('despacio.txt')
+
 function requestHandler(req, res) {
-    res.end('hi')
+  if (req.url == '/baby') {
+  res.end(data.toString())
+} else if (req.url == '/yellow') {
+res.end(lyrics.toString())
+} else if (req.url == '/despacio'){
+  res.end(espana.toString())
+}
+else {
+  res.end('Hi '+req.url)
+  }
 }
 var myServer = http.createServer(requestHandler)
 
