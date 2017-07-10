a577a79b1c59a34cf58ba1f70baa5ea2f8504600
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-10 16:42:14 -0700

    Remember the body parser

--- books_crud/index.js
+++ books_crud/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 //connect to mongodb database
 const DB_URL = 'mongodb://localhost/books_crud_app'
 
@@ -25,12 +27,12 @@ var bookSchema = new mongoose.Schema({
 const Book = mongoose.model('Book', bookSchema)
 
 // Create a test book
-Book.create({
-  name: 'Hamlet',
-  author: 'Shakespeare',
-  genre: 'drama',
-  isbn: 667
-})
+// Book.create({
+//   name: 'Hamlet',
+//   author: 'Shakespeare',
+//   genre: 'drama',
+//   isbn: 667
+// })
 
 //create a CRUD router for books
 //books index
@@ -41,21 +43,20 @@ app.get('/books', function(req,res) {
 })
 //show a book
 app.get('/books/:id', function(req, res){
-  console.log(req.params.id)
-  Book.findById(req.params.id, function(err, bookFound) {
+  Book.findById(req.params.id, function(err, books) {
     if(err) return console.log(err)
-    res.send(bookFound)
+    res.send(books)
   })
 })
 //create a book
 app.post('/books', function(req, res) {
-  Book.create(req.body, function (err, newBook) {
+  Book.create(req.body, function(err, newBook) {
     if(err) return console.log(err)
     res.json({message: "Book added!", book: newBook})
   })
 })
 
-//update a robot
+//update a book
 app.patch('/books/:id', function(req, res) {
   Book.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedBook) {
     if(err) return console.log(err)
@@ -68,6 +69,14 @@ app.delete('/books/:id', function(req, res) {
   Book.findByIdAndRemove(req.params.id, function(err, deletedBook) {
     if(err) return console.log(err)
     res.json({message: "Book Burned!", book: deletedBook})
+  })
+})
+
+//destroy them all!
+app.delete('/books', function(req, res) {
+  Book.remove({}, function(err, removedBooks){
+    if(err) return console.log(err)
+    res.json({message: "Books gone", book: removedBooks})
   })
 })
 
