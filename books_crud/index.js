414030a7756aca3ac74f003b040bd097a39b9855
Michael Mazzarella <michael_mazzarella@hotmail.com>
2017-07-10 15:24:28 -0700

    Getting it straightened out

--- books_crud/index.js
+++ books_crud/index.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
 
+//connect to mongodb database
 const DB_URL = 'mongodb://localhost/books_crud_app'
 
 mongoose.connect(DB_URL, function(err) {
@@ -28,3 +31,14 @@ Book.create({
   genre: 'drama',
   isbn: 667
 })
+
+app.get('/books', function(req,res) {
+  Book.find({}, function(err, allThemBooks) {
+    res.send(allThemBooks)
+  })
+})
+
+app.listen(port, function(err){
+  console.log(err || `Server running on ${port}`)
+})
+console.log('Michael')
